f2f0bc5978077c1e483b550ac123033d1397940b
bram <brammahendrawan693@gmail.com>
2023-06-28 23:58:40 +0700

    create routing request
    Signed-off-by: bram <brammahendrawan693@gmail.com>

--- routing_request/index.js
+++ routing_request/index.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
@@ -36,34 +34,4 @@ const requestListener = (request, response) => {
     } else {
         response.end('<h1>Halaman tidak dapat ditemukan!</h1>');
     }
-
-
-    // if (method === 'GET') {
-    //     response.end('<h1>Hello!</h1>');
-    // }
-
-    // if (method === 'POST') {
-    //     let body = [];
-
-    //     request.on('data', (chunk) => {
-    //         body.push(chunk);
-    //     });
-
-    //     request.on('end', () => {
-    //         body = Buffer.concat(body).toString();
-    //         const {
-    //             name
-    //         } = JSON.parse(body);
-    //         response.end(`<h1>Hai, ${name}!</h1>`);
-    //     });
-    // }
 };
-
-const server = http.createServer(requestListener);
-
-const port = 5000;
-const host = 'localhost';
-
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
