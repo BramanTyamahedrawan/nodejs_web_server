659ed744abac6577e87ff956754d7a33c116a720
bram <brammahendrawan693@gmail.com>
2023-06-29 00:13:52 +0700

    create response header
    Signed-off-by: bram <brammahendrawan693@gmail.com>

--- response_header/index.js
+++ response_header/index.js
@@ -1,8 +1,9 @@
-const http = require('http');
-
 const requestListener = (request, response) => {
+    // response merupakan objek yang dikirimkan server ke client
+    // berikut adalah perubahannya
     response.setHeader('Content-Type', 'application/json');
     response.setHeader('X-Powered-By', 'NodeJS');
+    // batas perubahan
     response.statusCode = 200;
 
     const {
@@ -38,37 +39,7 @@ const requestListener = (request, response) => {
             });
         }
     } else {
-        response.statusCode = 404;
+        response.statusCode = 400;
         response.end('<h1>Halaman tidak dapat ditemukan!</h1>');
     }
-
-
-    // if (method === 'GET') {
-    //     response.end('<h1>Hello!</h1>');
-    // }
-
-    // if (method === 'POST') {
-    //     let body = [];
-
-    //     request.on('data', (chunk) => {
-    //         body.push(chunk);
-    //     });
-
-    //     request.on('end', () => {
-    //         body = Buffer.concat(body).toString();
-    //         const {
-    //             name
-    //         } = JSON.parse(body);
-    //         response.end(`<h1>Hai, ${name}!</h1>`);
-    //     });
-    // }
 };
-
-const server = http.createServer(requestListener);
-
-const port = 5000;
-const host = 'localhost';
-
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
